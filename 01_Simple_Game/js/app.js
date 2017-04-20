3da591ce4ac7b2e73de4497560f6da9b3d26a817
amplejoe <amplejoe@gmail.com>
2017-04-20 10:36:43 +0200

    finished tutorial 2

--- 01_Simple_Game/js/app.js
+++ 01_Simple_Game/js/app.js
@@ -1,22 +1,62 @@
 window.onload = function()
 {
+
+    /** GLOBALS */
+
+    // game variable
     let game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
-    let logo;
+    // various
+    let player;
+    let platforms;
+    let cursors;
+    let jumpButton;
 
     function preload ()
     {
-        game.load.image('logo', 'assets/sprites/phaser.png');
+        game.load.image('player', 'assets/sprites/phaser-dude.png');
+        game.load.image('platform', 'assets/sprites/platform.png');
     }
 
     function create ()
     {
-        logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
+      player = game.add.sprite(100, 200, 'player');
+
+      game.physics.arcade.enable(player);
+
+      player.body.collideWorldBounds = true;
+      player.body.gravity.y = 500;
+
+      platforms = game.add.physicsGroup();
+
+      platforms.create(500, 150, 'platform');
+      platforms.create(-200, 300, 'platform');
+      platforms.create(400, 450, 'platform');
+
+      platforms.setAll('body.immovable', true);
+
+      cursors = game.input.keyboard.createCursorKeys();
+      jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     }
 
     function update ()
     {
-      logo.rotation += 0.01;
+      game.physics.arcade.collide(player, platforms);
+
+      player.body.velocity.x = 0;
+
+      if (cursors.left.isDown)
+      {
+          player.body.velocity.x = -250;
+      }
+      else if (cursors.right.isDown)
+      {
+          player.body.velocity.x = 250;
+      }
+
+      if (jumpButton.isDown && (player.body.onFloor() || player.body.touching.down))
+      {
+          player.body.velocity.y = -400;
+      }
     }
 };
