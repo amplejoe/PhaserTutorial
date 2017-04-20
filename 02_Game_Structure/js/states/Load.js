a104cb033e316b8575f6759b3197fbdd74679cda
anleibetsed <aleibets@itec.aau.at>
2017-04-21 01:40:23 +0200

    updated big asset loading for home internet speeds

--- 02_Game_Structure/js/states/Load.js
+++ 02_Game_Structure/js/states/Load.js
@@ -22,14 +22,15 @@ Tutorial.Load.prototype =
     this.load.image('logo', 'assets/sprites/phaser.png');
     this.load.image('player', 'assets/sprites/phaser-dude.png');
     this.load.image('platform', 'assets/sprites/platform.png');
-    // load some big assets to slow down loading screen (requires internet)
-    this.load.image('uhq1',
-      'https://upload.wikimedia.org/wikipedia/commons/0/0b/Alte_Mainbr%C3%BCcke%2C_W%C3%BCrzburg%2C_North-West_View_20140604_1.jpg');
-    this.load.image('uhq2',
-      'https://upload.wikimedia.org/wikipedia/commons/3/38/Chicago_sunrise_3.jpg');
-    this.load.image('uhq3',
-      'https://upload.wikimedia.org/wikipedia/commons/3/3b/Paris-pano-wladyslaw.jpg');
-
+    // load some bigger assets to slow down loading screen (requires internet)
+    this.load.image('hq1',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Phanom_Rung_Wikimedia_Commons.jpg/1280px-Phanom_Rung_Wikimedia_Commons.jpg');
+    this.load.image('hq2',
+      'https://upload.wikimedia.org/wikipedia/commons/a/a6/World_Bank_building_at_Washington.jpg');
+    this.load.image('hq3',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Lake_Kinney_mit_Mount_Whitehorn.jpg/1024px-Lake_Kinney_mit_Mount_Whitehorn.jpg');
+    this.load.image('hq4',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Cappadocia_Chimneys_Wikimedia_Commons.jpg/1024px-Cappadocia_Chimneys_Wikimedia_Commons.jpg');
   },
   create: function()
   {
