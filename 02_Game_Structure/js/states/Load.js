37f115aeb6314b088397bf0c0f3bb2c549c2c504
anleibetsed <aleibets@itec.aau.at>
2017-04-21 02:03:03 +0200

    better method for increasing loading times via loremflickr

--- 02_Game_Structure/js/states/Load.js
+++ 02_Game_Structure/js/states/Load.js
@@ -14,7 +14,7 @@ Tutorial.Load.prototype =
 
     // progress bar
     var progressBar = this.add.sprite(this.world.centerX - (0.5 * this.cache.getImage('progressBar').width),
-      this.world.centerY + 50, 'progressBar');
+      this.world.centerY + 30, 'progressBar');
     //progressBar.anchor.setTo(0.5, 0.5);
     this.load.setPreloadSprite(progressBar); // automatically scales progress bar
 
@@ -22,15 +22,12 @@ Tutorial.Load.prototype =
     this.load.image('logo', 'assets/sprites/phaser.png');
     this.load.image('player', 'assets/sprites/phaser-dude.png');
     this.load.image('platform', 'assets/sprites/platform.png');
-    // load some bigger assets to slow down loading screen (requires internet)
-    this.load.image('hq1',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Phanom_Rung_Wikimedia_Commons.jpg/1280px-Phanom_Rung_Wikimedia_Commons.jpg');
-    this.load.image('hq2',
-      'https://upload.wikimedia.org/wikipedia/commons/a/a6/World_Bank_building_at_Washington.jpg');
-    this.load.image('hq3',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Lake_Kinney_mit_Mount_Whitehorn.jpg/1024px-Lake_Kinney_mit_Mount_Whitehorn.jpg');
-    this.load.image('hq4',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Cappadocia_Chimneys_Wikimedia_Commons.jpg/1024px-Cappadocia_Chimneys_Wikimedia_Commons.jpg');
+    // load some assets to slow down loading screen (requires internet)
+    numImages = 10 // increase until you see the progress bar
+    for (let i=0;i<numImages;i++)
+      this.load.image('dummy'+i, "http://loremflickr.com/"+(320+i)+"/"+(240+i));
+
+
   },
   create: function()
   {
