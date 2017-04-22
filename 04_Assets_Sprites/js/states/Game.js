fb8d8b7dcdf4851dea29f5d6a269b2d314b6e815
amplejoe <amplejoe@gmail.com>
2017-04-22 23:10:07 +0200

    improved t04

--- 04_Assets_Sprites/js/states/Game.js
+++ 04_Assets_Sprites/js/states/Game.js
@@ -2,7 +2,6 @@
 Tutorial.Game = function (game)
 {
   /* members */
-
 };
 
 Tutorial.Game.prototype =
@@ -16,7 +15,17 @@ Tutorial.Game.prototype =
     // adding assets
 
     // plain picture
-    this.logo = this.add.sprite(0, 0, 'logo');
+    let logo = this.add.sprite(0, 0, 'logo');
+
+    // non mapped spritesheet
+    let chest_1 = this.add.sprite(this.world.centerX, 10,'chests', 0); // 1st sprite
+    let chest_2 = this.add.sprite(this.world.centerX, 50,'chests', 4); // 5th sprite
+    let chest_3 = this.add.sprite(this.world.centerX, 90,'chests', 7); // 8th sprite
+
+
+    // TODO: named sprite -> frame = number|string - for maps?
+    //let chest_2 = this.add.sprite(20, 40,'chests', "name"); // nam sprite
+
 
     //this.character = this.add.sprite(this.world.centerX, this.world.centerY, 'character');
 
