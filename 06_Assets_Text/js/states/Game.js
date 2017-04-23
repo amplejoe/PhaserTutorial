e1bb50959874c506a8618abb5429e6d832fc9274
amplejoe <amplejoe@gmail.com>
2017-04-23 16:54:07 +0200

    projects for tutorials 6-8

---
+++ 06_Assets_Text/js/states/Game.js
@@ -0,0 +1,23 @@
+
+Tutorial.Game = function (game)
+{
+  /* members */
+  this.showDebug=false;
+  this.toggleDebugButton;
+  this.DebugRect;
+};
+
+Tutorial.Game.prototype =
+{
+  create: function ()
+  {
+
+
+  },
+  render: function()
+  {
+    // debug text output
+    this.game.debug.text( "Debug Text.", 5, Tutorial.SCREEN_HEIGHT - 10 );
+
+  }
+};
---
+++ 08_Input/js/globals.js
@@ -0,0 +1,9 @@
+
+/** global namespace **/
+let Tutorial = {};
+
+/** constants **/
+Tutorial.SCREEN_WIDTH = 800;
+Tutorial.SCREEN_HEIGHT = 600;
+// option for 2 tileset loading variants: "JSON" or "CSV"
+Tutorial.TILEMAP_TYPE = "JSON";
